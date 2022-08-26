621a506b2a7c559bb69f36eead89f47efec31626
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_guac/ubuntu/__init__.py
+++ offregister_guac/ubuntu/__init__.py
@@ -1,9 +1,7 @@
 from os import path
 from sys import modules
 
-from fabric.context_managers import cd
-from fabric.contrib.files import append, exists, upload_template
-from fabric.operations import run, sudo
+from fabric.contrib.files import append, exists
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.fs import cmd_avail
 from pkg_resources import resource_filename
@@ -21,12 +19,13 @@ def install0(*args, **kwargs):
 
 
 def configure_tomcat1(*args, **kwargs):
-    CATALINA_HOME = run("echo -n $CATALINA_HOME")
+    CATALINA_HOME = c.run("echo -n $CATALINA_HOME").stdout.rstrip()
     tomcat_users_local_filepath = kwargs.get(
         "tomcat-users.xml",
         resource_filename(PKG_NAME, path.join("tomcat_conf", "tomcat-users.xml")),
     )
-    upload_template(
+    upload_template_fmt(
+        c,
         tomcat_users_local_filepath,
         "{CATALINA_HOME}/conf/tomcat-users.xml".format(CATALINA_HOME=CATALINA_HOME),
         context={
@@ -36,48 +35,50 @@ def configure_tomcat1(*args, **kwargs):
         use_sudo=True,
     )
 
-    with cd(CATALINA_HOME):
+    with c.cd(CATALINA_HOME):
         """
         dae = 'commons-daemon-native'
-        sudo('tar xf {dae}.tar.gz -C {dae} --strip-components 1'.format(dae=dae))
-        with cd(dae):
-            sudo('./configure')
-            sudo('make')
-            sudo('cp jsvc ../..')
+        c.sudo('tar xf {dae}.tar.gz -C {dae} --strip-components 1'.format(dae=dae))
+        with c.cd(dae):
+            c.sudo('./configure')
+            c.sudo('make')
+            c.sudo('cp jsvc ../..')
         """
-        if run("id -u tomcat", warn_only=True, quiet=True).failed:
-            sudo("groupadd tomcat")
-            sudo(
+        if c.run("id -u tomcat", warn=True, hide=True).exited != 0:
+            c.sudo("groupadd tomcat")
+            c.sudo(
                 "useradd -s /bin/false -g tomcat -d {CATALINA_HOME} tomcat".format(
                     CATALINA_HOME=CATALINA_HOME
                 )
             )
-        sudo("chgrp -R tomcat {CATALINA_HOME}".format(CATALINA_HOME=CATALINA_HOME))
-        sudo("chmod -R g+r conf")
-        sudo("chmod g+x conf")
-        sudo("chown -R tomcat webapps/ work/ temp/ logs/")
+        c.sudo("chgrp -R tomcat {CATALINA_HOME}".format(CATALINA_HOME=CATALINA_HOME))
+        c.sudo("chmod -R g+r conf")
+        c.sudo("chmod g+x conf")
+        c.sudo("chown -R tomcat webapps/ work/ temp/ logs/")
 
         tomcat_service_local_filepath = kwargs.get(
             "tomcat.service",
             resource_filename(PKG_NAME, path.join("_data", "tomcat.service")),
         )
-        upload_template(
+        upload_template_fmt(
+            c,
             tomcat_service_local_filepath,
             "/etc/systemd/system/tomcat.service",
             context=dict(CATALINA_HOME=CATALINA_HOME),
             use_sudo=True,
         )
-        return sudo("systemctl daemon-reload")
+        return c.sudo("systemctl daemon-reload")
 
 
 def serve2(*args, **kwargs):
-    sudo("systemctl start tomcat")
-    return sudo("/etc/init.d/guacd start")
+    c.sudo("systemctl start tomcat")
+    return c.sudo("/etc/init.d/guacd start")
 
 
 def install_deps():
-    # sudo('add-apt-repository ppa:mc3man/trusty-media')
+    # c.sudo('add-apt-repository ppa:mc3man/trusty-media')
     apt_depends(
+        c,
         "curl",
         # From  Guacamole
         "libcairo2-dev",  # 'libjpeg62-turbo-dev', 'libjpeg62-dev',
@@ -125,93 +126,95 @@ def install_deps():
         # From ffmpeg
         "yasm",
     )
-    sudo(
+    c.sudo(
         "update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 1"
     )
-    sudo(
+    c.sudo(
         "update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac 1"
     )
 
-    sudo("mkdir -p ~/Downloads")
-    curr_ug = run("echo -n $USER:$GROUP", quiet=True)
-    sudo("chown -R {curr_ug} ~/Downloads".format(curr_ug=curr_ug))
-    with cd("Downloads"):
+    c.sudo("mkdir -p ~/Downloads")
+    curr_ug = c.run("echo -n $USER:$GROUP", hide=True).stdout.rstrip()
+    c.sudo("chown -R {curr_ug} ~/Downloads".format(curr_ug=curr_ug))
+    with c.cd("Downloads"):
         libjpeg = "libjpeg-turbo-official_1.5.2_amd64.deb"
-        if not exists(libjpeg):
-            run(
+        if not exists(c, runner=c.run, path=libjpeg):
+            c.run(
                 "curl -L 'https://downloads.sourceforge.net/project/libjpeg-turbo/{ver}/{pkg}' -o '{pkg}'".format(
                     pkg=libjpeg, ver=libjpeg.rpartition("_")[0].partition("_")[2]
                 )
             )
-            sudo("dpkg -i {pkg}".format(pkg=libjpeg))
+            c.sudo("dpkg -i {pkg}".format(pkg=libjpeg))
 
         zlib = "zlib-1.2.11"
-        if not exists(zlib):
-            run("curl -OL http://www.zlib.net/{pkg}.tar.gz".format(pkg=zlib))
-            run("tar xf {pkg}.tar.gz".format(pkg=zlib))
-            with cd(zlib):
-                run("./configure")
-                run("make")
-                sudo("make install")
+        if not exists(c, runner=c.run, path=zlib):
+            c.run("curl -OL http://www.zlib.net/{pkg}.tar.gz".format(pkg=zlib))
+            c.run("tar xf {pkg}.tar.gz".format(pkg=zlib))
+            with c.cd(zlib):
+                c.run("./configure")
+                c.run("make")
+                c.sudo("make install")
 
         libvnc = "LibVNCServer-0.9.11"
-        if not exists("{pkg}.tar.gz".format(pkg=libvnc)):
-            run(
+        if not exists(c, runner=c.run, path="{pkg}.tar.gz".format(pkg=libvnc)):
+            c.run(
                 "curl -OL https://github.com/LibVNC/libvncserver/archive/{pkg}.tar.gz".format(
                     pkg=libvnc
                 )
             )
-            run("tar xf {pkg}.tar.gz".format(pkg=libvnc))
-            with cd("libvncserver-{pkg}".format(pkg=libvnc)):
-                run("mkdir build")
-                with cd("build"):
-                    run("cmake ..")
-                    run("cmake --build .")
-                    sudo("make install")
+            c.run("tar xf {pkg}.tar.gz".format(pkg=libvnc))
+            with c.cd("libvncserver-{pkg}".format(pkg=libvnc)):
+                c.run("mkdir build")
+                with c.cd("build"):
+                    c.run("cmake ..")
+                    c.run("cmake --build .")
+                    c.sudo("make install")
 
         """freerdp = '2.0.0-rc0'
-        run('curl -OL https://github.com/FreeRDP/FreeRDP/archive/{pkg}.tar.gz'.format(pkg=freerdp))
-        run('tar xf {pkg}.tar.gz'.format(pkg=freerdp))
-        with cd('FreeRDP-{pkg}'.format(pkg=freerdp)):
-            run('rm -rf build')
-            run('mkdir build')
-            with cd('build'):
-                run('cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SSE2=ON ..')
-                run('make')
-                sudo('make install')
-                sudo('mkdir -p /etc/ld.so.conf.d')
+        c.run('curl -OL https://github.com/FreeRDP/FreeRDP/archive/{pkg}.tar.gz'.format(pkg=freerdp))
+        c.run('tar xf {pkg}.tar.gz'.format(pkg=freerdp))
+        with c.cd('FreeRDP-{pkg}'.format(pkg=freerdp)):
+            c.run('rm -rf build')
+            c.run('mkdir build')
+            with c.cd('build'):
+                c.run('cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SSE2=ON ..')
+                c.run('make')
+                c.sudo('make install')
+                c.sudo('mkdir -p /etc/ld.so.conf.d')
                 append('/etc/ld.so.conf.d/freerdp.conf', '/usr/local/lib/freerdp', use_sudo=True)
-                sudo('ldconfig')"""
+                c.sudo('ldconfig')"""
 
         libtelnet = "libtelnet-master"
-        if not exists(libtelnet):
-            run(
+        if not exists(c, runner=c.run, path=libtelnet):
+            c.run(
                 "curl -L https://github.com/seanmiddleditch/libtelnet/archive/master.tar.gz -o {pkg}.tar.gz".format(
                     pkg=libtelnet
                 )
             )
-            run("tar xf {pkg}.tar.gz".format(pkg=libtelnet))
-            with cd(libtelnet):
-                run("autoreconf -fi")
-                run(
+            c.run("tar xf {pkg}.tar.gz".format(pkg=libtelnet))
+            with c.cd(libtelnet):
+                c.run("autoreconf -fi")
+                c.run(
                     'zlib_CFLAGS="-DHAVE_ZLIB=1" zlib_LIBS="-lz" ./configure --disable-util'
                 )
-                run("make")
-                sudo("make install")
-                sudo("ldconfig")
+                c.run("make")
+                c.sudo("make install")
+                c.sudo("ldconfig")
 
-        if not cmd_avail("ffmpeg"):
+        if not cmd_avail(c, "ffmpeg"):
             ffmpeg = "ffmpeg-3.3.4"
-            run("curl -OL http://ffmpeg.org/releases/{pkg}.tar.bz2".format(pkg=ffmpeg))
-            run("tar xf {pkg}.tar.bz2".format(pkg=ffmpeg))
-            with cd(ffmpeg):
-                run("./configure")
-                run("make")
-                sudo("make install")
+            c.run(
+                "curl -OL http://ffmpeg.org/releases/{pkg}.tar.bz2".format(pkg=ffmpeg)
+            )
+            c.run("tar xf {pkg}.tar.bz2".format(pkg=ffmpeg))
+            with c.cd(ffmpeg):
+                c.run("./configure")
+                c.run("make")
+                c.sudo("make install")
 
         CATALINA_HOME = "/opt/javalibs/tomcat9"
-        if not exists(CATALINA_HOME):
-            sudo("mkdir -p {CATALINA_HOME}".format(CATALINA_HOME=CATALINA_HOME))
+        if not exists(c, runner=c.run, path=CATALINA_HOME):
+            c.sudo("mkdir -p {CATALINA_HOME}".format(CATALINA_HOME=CATALINA_HOME))
             append(
                 "/etc/environment",
                 "export CATALINA_HOME='{CATALINA_HOME}'".format(
@@ -221,12 +224,12 @@ def install_deps():
             )
             tomcat = "apache-tomcat-9.0.1"
             tomcat_ver = tomcat.rpartition("-")[2]
-            run(
+            c.run(
                 "curl -OL http://mirror.intergrid.com.au/apache/tomcat/tomcat-{ver[0]}/v{ver}/bin/{pkg}.tar.gz".format(
                     pkg=tomcat, ver=tomcat_ver
                 )
             )
-            sudo(
+            c.sudo(
                 "tar xf {pkg}.tar.gz -C {CATALINA_HOME} --strip-components=1".format(
                     pkg=tomcat, CATALINA_HOME=CATALINA_HOME
                 )
@@ -236,56 +239,53 @@ def install_deps():
 
 
 def install_guac_server():
-    if cmd_avail("guacenc"):
+    if cmd_avail(c, "guacenc"):
         return "Apache Guacamole server already installed"
 
-    with cd("Downloads"):
+    with c.cd("Downloads"):
         guac = "guacamole-server-0.9.13-incubating"
-        run(
+        c.run(
             "curl -OL 'http://apache.org/dyn/closer.cgi?action=download&filename=incubator/guacamole/0.9.13-incubating/source/{pkg}.tar.gz'".format(
                 pkg=guac
             )
         )
-        run("tar xf {pkg}.tar.gz".format(pkg=guac))
-        with cd(guac):
-            run("autoreconf -fi")
-            run("./configure --with-init-dir=/etc/init.d")
-            run("make")
-            sudo("make install")
-            sudo("ldconfig")
+        c.run("tar xf {pkg}.tar.gz".format(pkg=guac))
+        with c.cd(guac):
+            c.run("autoreconf -fi")
+            c.run("./configure --with-init-dir=/etc/init.d")
+            c.run("make")
+            c.sudo("make install")
+            c.sudo("ldconfig")
 
     return "installed Apache Guacamole server"
 
 
 def install_guac_client():
-    with cd("Downloads"):
-        if not cmd_avail("mvn"):
+    with c.cd("Downloads"):
+        if not cmd_avail(c, "mvn"):
             maven = "apache-maven-3.5.0-bin"
-            run(
+            c.run(
                 "curl -OL http://apache.melbourneitmirror.net/maven/maven-{ver[0]}/{ver}/binaries/{pkg}.tar.gz".format(
                     pkg=maven, ver=maven.rpartition("-")[0].rpartition("-")[2]
                 )
             )
             d = "/opt/javalibs/maven"
-            sudo("mkdir -p {d}".format(d=d))
-            sudo(
+            c.sudo("mkdir -p {d}".format(d=d))
+            c.sudo(
                 "tar xf {pkg}.tar.gz -C {d} --strip-components=1".format(pkg=maven, d=d)
             )
-            with cd(d):
-                sudo(
-                    "ln -s '{d}/bin/mvn' '/usr/local/bin/$b'".format(d=d),
-                    shell_escape=False,
-                )
+            with c.cd(d):
+                c.sudo("ln -s '{d}/bin/mvn' '/usr/local/bin/$b'".format(d=d))
         guac_client = "0.9.13-incubating"
-        if not exists("{pkg}.tar.gz".format(pkg=guac_client)):
-            run(
+        if not exists(c, runner=c.run, path="{pkg}.tar.gz".format(pkg=guac_client)):
+            c.run(
                 "curl -OL https://github.com/apache/incubator-guacamole-client/archive/{pkg}.tar.gz".format(
                     pkg=guac_client
                 )
             )
-            run("tar xf {pkg}.tar.gz".format(pkg=guac_client))
-        with cd("incubator-guacamole-client-{pkg}".format(pkg=guac_client)):
+            c.run("tar xf {pkg}.tar.gz".format(pkg=guac_client))
+        with c.cd("incubator-guacamole-client-{pkg}".format(pkg=guac_client)):
             war = "guacamole/target/guacamole-0.9.13-incubating.war"
-            if not exists(war):
-                run("mvn package")
-            sudo("cp {war} $CATALINA_HOME/webapps".format(war=war))
+            if not exists(c, runner=c.run, path=war):
+                c.run("mvn package")
+            c.sudo("cp {war} $CATALINA_HOME/webapps".format(war=war))
